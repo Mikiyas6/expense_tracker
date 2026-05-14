ef30b857019d45462ad4813b401b9c6a86e6c4d9
Mikiyas6 <Mikitedi9@gmail.com>
2026-05-14 21:38:24 +0300

    Add ExpensesList widget and update Expense model to include amount and date

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,5 @@
+import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -6,11 +8,30 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: "Flutter Course",
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: "Cinema",
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [Text("Chartsssssssssssss"), Text("Expenses List")],
+      body: Center(
+        child: Column(
+          children: [
+            Text("Chartsssssssssssss"),
+            Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+          ],
+        ),
       ),
     );
   }
