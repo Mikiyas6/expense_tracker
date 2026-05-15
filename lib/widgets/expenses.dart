23f31170f0ff3eee84cd44fde1de8424241a6da0
Mikiyas6 <Mikitedi9@gmail.com>
2026-05-15 23:59:58 +0300

    Refactor expenses management: move Expenses and ExpensesList to widgets, implement ExpenseItem for better UI representation

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,4 @@
-import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
@@ -25,13 +25,11 @@ class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            Text("Chartsssssssssssss"),
-            Expanded(child: ExpensesList(expenses: _registeredExpenses)),
-          ],
-        ),
+      body: Column(
+        children: [
+          Text("Chartsssssssssssss"),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+        ],
       ),
     );
   }
