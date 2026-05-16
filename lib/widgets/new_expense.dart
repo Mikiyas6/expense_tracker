55d85765e632e1a9c1549e87ad7ca02255d83480
Mikiyas6 <Mikitedi9@gmail.com>
2026-05-16 22:38:09 +0300

    Implement NewExpense widget: add title and amount input fields with save functionality

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -7,6 +7,23 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  late TextEditingController _titleController;
+  late TextEditingController _amountController;
+
+  @override
+  void initState() {
+    _titleController = TextEditingController();
+    _amountController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -14,10 +31,30 @@ class _NewExpenseState extends State<NewExpense> {
       child: Column(
         children: [
           TextField(
+            controller: _titleController,
             keyboardType: TextInputType.text,
             maxLength: 50,
             decoration: InputDecoration(label: Text("Title")),
-            autocorrect: true,
+          ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              label: Text("Amount"),
+              prefixText: '\$',
+            ),
+          ),
+          Row(
+            children: [
+              TextButton(onPressed: () {}, child: Text("Cancel")),
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_amountController.text);
+                },
+                child: Text("Save Expense"),
+              ),
+            ],
           ),
         ],
       ),
