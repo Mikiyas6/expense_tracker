1a610df749fbe5377ff31b7d70e0910caa7f0b3d
Mikiyas6 <Mikitedi9@gmail.com>
2026-05-28 22:19:33 +0300

    Add dark theme support with customized color scheme and button styles

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,33 @@ import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 var kColorScheme = ColorScheme.fromSeed(seedColor: Colors.deepPurple);
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: Color.fromARGB(255, 5, 9, 125),
+);
 
 void main() {
   runApp(
     DevicePreview(
       enabled: !const bool.fromEnvironment('dart.vm.product'),
       builder: (context) => MaterialApp(
+        themeMode: ThemeMode.dark,
         home: Expenses(),
         useInheritedMediaQuery: true,
         locale: DevicePreview.locale(context),
+        darkTheme: ThemeData.dark().copyWith(
+          colorScheme: kDarkColorScheme,
+          cardTheme: CardThemeData().copyWith(
+            color: kDarkColorScheme.secondaryContainer,
+            margin: EdgeInsets.all(12),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kDarkColorScheme.primaryContainer,
+              foregroundColor: kDarkColorScheme.onPrimaryContainer,
+            ),
+          ),
+        ),
         theme: ThemeData().copyWith(
           colorScheme: kColorScheme,
           appBarTheme: AppBarTheme().copyWith(
